5e05a14da6132e43f4e86c2d2511a7dce95f0ada
Александр <avsimvolokov@gmail.com>
2023-10-31 20:55:35 +0300

    Review code

--- BuildingUIKitProgrammatically002/ViewController.swift
+++ BuildingUIKitProgrammatically002/ViewController.swift
@@ -7,34 +7,41 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ViewController: UIViewController {//, UITableViewDelegate, UITableViewDataSource {
 
-    let myArray: NSArray = ["First","Second","Third"]
-    var myTableView: UITableView!
-
+    let myArray = ["First","Second","Third"]
+    let myTableView = UITableView()
+    let cellIdentifier = "MyCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let barHeight: CGFloat = UIApplication.shared.statusBarFrame.size.height
-        //let barHeight: CGFloat = UIApplication.shared.statusBarManager.size.height
-        let displayWidth: CGFloat = self.view.frame.width
-        let displayHeight: CGFloat = self.view.frame.height
+        view.backgroundColor = .yellow
+        
+        view.addSubview(myTableView)
+        
+        myTableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        myTableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        myTableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        myTableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        myTableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
 
-        myTableView = UITableView(frame: CGRect(x: 0, y: barHeight, width: displayWidth, height: displayHeight - barHeight))
-//        myTableView = UITableView(frame: CGRect(x: 0, y: 0, width: displayWidth, height: displayHeight - 0))
-        //myTableView = UITableView(frame: c)
-        myTableView.register(UITableViewCell.self, forCellReuseIdentifier: "MyCell")
+        myTableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+        
         myTableView.dataSource = self
         myTableView.delegate = self
-        view.addSubview(myTableView)
+        
+        
+        myTableView.backgroundColor = .brown
     }
 
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("Num: \(indexPath.row)")
-        print("Value: \(myArray[indexPath.row])")
-    }
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        print("Num: \(indexPath.row)")
+//        print("Value: \(myArray[indexPath.row])")
+//    }
 
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    /*func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return myArray.count
     }
 
@@ -42,6 +49,23 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath as IndexPath)
         cell.textLabel!.text = "\(myArray[indexPath.row])"
         return cell
-    }
+    }*/
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        myArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath as IndexPath)
+        cell.textLabel!.text = "\(myArray[indexPath.row])"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Num: \(indexPath.row)")
+        print("Value: \(myArray[indexPath.row])")
+    }
+    
+}
